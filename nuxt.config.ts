9bacca590a2cc13388c564a6158903da151cc985
impeachYoon <corbels-fallout.7h@icloud.com>
2024-12-06 23:52:32 +0900

    .

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -9,19 +9,19 @@ export default defineNuxtConfig({
       charset: 'utf-8',
       meta: [
         {
-          name: "title",
+          property: "title",
           content: "국민의힘 단체문자 매크로"
         },
         {
-          name: "og:title",
+          property: "og:title",
           content: "국민의힘 단체문자 매크로"
         },
         {
-          name: "description",
+          property: "description",
           content: "국민의힘 의원들에게 우리 같이 윤석열을 탄핵시킬 용기를 불어넣어줘요."
         },
         {
-          name: "og:description",
+          property: "og:description",
           content: "국민의힘 의원들에게 우리 같이 윤석열을 탄핵시킬 용기를 불어넣어줘요."
         }
       ]
@@ -30,11 +30,4 @@ export default defineNuxtConfig({
 
   modules: ["@nuxt/ui"],
 
-  colorMode: {
-    preference: "dark"
-  },
-  ui: {
-    primary: "neutral",
-    gray: "neutral"
-  }
 })
